63289ebdbe68d765a7ae6a266d6a44343739908e
aymen ben nasr <aymen.bennasr@sesame.com.tn>
2021-02-21 10:59:27 +0100

    first commit

---
+++ src/main/java/com/sip/ams/AmsApplication.java
@@ -0,0 +1,18 @@
+package com.sip.ams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class AmsApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		//String ch="Spring";
+		//System.out.println(ch);
+		//ch = ch.concat("Boot");
+		//System.out.println(ch);
+		SpringApplication.run(AmsApplication.class, args);
+	}
+
+}
