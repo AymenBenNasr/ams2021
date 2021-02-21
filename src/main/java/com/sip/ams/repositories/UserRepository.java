84865c791864823c5f9f92aeea419cb69128587c
aymen ben nasr <aymen.bennasr@sesame.com.tn>
2021-02-21 11:44:02 +0100

    last commit

--- src/main/java/com/sip/ams/repositories/UserRepository.java
+++ src/main/java/com/sip/ams/repositories/UserRepository.java
@@ -2,7 +2,6 @@ package com.sip.ams.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.sip.ams.entities.User;
 import com.sip.ams.entities.Users;
 
 @Repository("userRepository")
